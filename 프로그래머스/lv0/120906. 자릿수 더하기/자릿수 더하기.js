95d252d5fb175525427135a9511480da9558f0dd
lionloopy <121242536+lionloopy@users.noreply.github.com>
2023-01-27 00:52:40 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.js
@@ -1,6 +1,6 @@
 function solution(n) {
     var answer = 0;
-    n = String(n).split('')
+    n = String(n).split('') // [...(''+n)]
     let sum = 0
     for(let i=0; i<n.length; i++){
         sum += +n[i]
