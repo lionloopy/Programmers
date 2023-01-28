29b5a7abf7754379cbebc5cad6056f33eb82a017
lionloopy <121242536+lionloopy@users.noreply.github.com>
2023-01-28 19:54:43 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,10 @@
+function solution(num) {
+    var answer = 0;
+    
+    while( num !== 1){
+        if(answer >500) return -1
+        num%2 ==0? num=num/2 : num=num*3+1
+        answer ++
+    }
+    return answer;
+}
