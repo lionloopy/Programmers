3ed24db2792518462e8f0489a487dcee2cd4e32e
lionloopy <121242536+lionloopy@users.noreply.github.com>
2023-01-30 20:09:12 +0900

    [level 1] Title: 행렬의 덧셈, Time: 8.18 ms, Memory: 67.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.js
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -1,11 +1,12 @@
 function solution(arr1, arr2) {
     var answer = [];
-    for(let i=0; i<arr1.length;i++){
+    for(let i=0; i<arr1.length;i++){ //첫번째 for문은 겉배열
         let sum = []
-        for(let j=0; j<arr1[i].length;j++){
+        for(let j=0; j<arr1[i].length;j++){ //두번째 for문은 속배열
             sum.push(arr1[i][j] + arr2[i][j])
         }
         answer.push(sum)
+        //먼저 []배열에 넣어주고 이 배열을 answer배열에 넣어주면서 이중 배열을 만든다.
     }
     return answer;
 }
