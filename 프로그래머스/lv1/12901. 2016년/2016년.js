1dc434b0b763cc84e258c6e569adf7cac411e6e0
lionloopy <121242536+lionloopy@users.noreply.github.com>
2023-01-30 09:39:38 +0900

    [level 1] Title: 2016년, Time: 0.25 ms, Memory: 34.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.js
+++ 프로그래머스/lv1/12901. 2016년/2016년.js
@@ -1,10 +1,11 @@
 function solution(a, b) {
-    var answer = 0;
-    let day = ['THU','FRI','SAT','SUN','MON','TUE','WED']
-let date = [31,29,31,30,31,30,31,31,30,31,30,31]
+    return new Date(2016, a-1, b).toString().slice(0,3).toUpperCase()
+//     var answer = 0;
+//     let day = ['THU','FRI','SAT','SUN','MON','TUE','WED']
+// let date = [31,29,31,30,31,30,31,31,30,31,30,31]
 
-for(let i =1; i<a; i++){
-  answer +=date[i-1]
-}
-return day[(answer+b)%7]
+// for(let i =1; i<a; i++){
+//   answer +=date[i-1]
+// }
+// return day[(answer+b)%7]
 }
