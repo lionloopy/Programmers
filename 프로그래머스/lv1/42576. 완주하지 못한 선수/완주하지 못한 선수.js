5d2157328f49fff2af952a2ab6289ee709378115
lionloopy <121242536+lionloopy@users.noreply.github.com>
2023-02-01 22:49:39 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 101.47 ms, Memory: 56.9 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,11 +1,10 @@
 function solution(participant, completion) {
-
-
-participant.sort();
-completion.sort();
-for (let i = 0; i < participant.length; i++) {
-  if (participant[i] !== completion[i]) {
-    return participant[i];
-  }
-}
+    var answer = '';
+    participant.sort()
+    completion.sort()
+    for(let i=0; i<participant.length; i++){
+        if(participant[i] !== completion[i]){
+            return participant[i]
+        }
+    }
 }
