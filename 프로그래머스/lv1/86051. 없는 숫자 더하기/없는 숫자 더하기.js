2d2a4b4751d886f836a0881960a5075c712dc4cf
lionloopy <121242536+lionloopy@users.noreply.github.com>
2023-02-01 20:13:08 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,9 +1,7 @@
 function solution(numbers) {
-    var answer = 0;
-    let all = [1,2,3,4,5,6,7,8,9,0]
-    let a = all.filter((x) => !numbers.includes(x))
-    for(let i =0; i<a.length;i++){
-        answer += a[i]
-    }
+    var answer = -1;
+    let all = [0,1,2,3,4,5,6,7,8,9]
+    let a = all.filter(x => !numbers.includes(x))
+    answer = a.reduce((a,b) => a+b,0)
     return answer;
 }
