0c9fca7eeff64d2bc4a61d5fff068551e996535d
lionloopy <121242536+lionloopy@users.noreply.github.com>
2023-02-01 23:33:43 +0900

    [level 1] Title: 하샤드 수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
@@ -1,11 +1,6 @@
 function solution(x) {
     var answer = true;
-    x = String(x);
-let sum = 0;
-for (let i = 0; i < x.length; i++) {
-  sum += parseInt(x[i]);
-}
-
-x%sum ==0? answer = true:answer=false
+    let a = String(x).split('').reduce((a,b) =>parseInt(a)+parseInt(b),0)
+    x%a == 0? answer = true: answer= false
     return answer;
 }
