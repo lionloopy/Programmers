5785f01e5ff39fc1e4ea037b85459dabfcfe2d1c
lionloopy <121242536+lionloopy@users.noreply.github.com>
2023-02-01 22:36:56 +0900

    [level 1] Title: 내적, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/70128. 내적/내적.js
+++ 프로그래머스/lv1/70128. 내적/내적.js
@@ -1,8 +1,9 @@
 function solution(a, b) {
-    var answer = [];
+    var answer = 1234567890;
+    let sum = []
     for(let i=0; i<a.length; i++){
-        answer.push(a[i] * b[i])
+        sum.push(a[i]*b[i])
     }
-    answer = answer.reduce((a,b) => a+b,0)
+    answer = sum.reduce((a,b) => a+b,0)
     return answer;
 }
