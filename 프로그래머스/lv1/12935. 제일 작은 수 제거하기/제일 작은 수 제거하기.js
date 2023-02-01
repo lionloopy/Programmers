abf1f79d8463cb3ee9862ff1f6c46aa98da53e65
lionloopy <121242536+lionloopy@users.noreply.github.com>
2023-02-01 23:23:14 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,6 +1,7 @@
 function solution(arr) {
-
+    var answer = [];
     arr.splice(arr.indexOf(Math.min(...arr)),1)
-    arr.length <2 ? answer = [-1] : answer = arr
-    return answer
+    arr.length < 2 ? answer.push(-1) : answer = arr
+    
+    return answer;
 }
