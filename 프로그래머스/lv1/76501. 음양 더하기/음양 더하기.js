6e3c80b62f7ac54aa12f189aa2855c59f8822cfd
lionloopy <121242536+lionloopy@users.noreply.github.com>
2023-02-01 20:24:10 +0900

    [level 1] Title: 음양 더하기, Time: 0.48 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
@@ -1,15 +1,16 @@
 function solution(absolutes, signs) {
-    var answer = 0;
-    for(let i=0; i<absolutes.length;i++){
-        if(signs[i] ===true){
-            absolutes[i] = absolutes[i]
-        }else{
-            absolutes[i] = -absolutes[i]
-        }
+
+    let sum = []
+for(let i =0; i<signs.length;i++){
+    if(signs[i] == true){
+        signs[i] = 1
+    }else if(signs[i] == false){
+        signs[i] = -1
     }
-    answer = absolutes.reduce((a,b) => a+b, 0)
+}
+for(let i=0; i<absolutes.length; i++){
+    sum.push(absolutes[i] * signs[i])
+}
+let answer = sum.reduce((a,b) => a+b,0)
     return answer;
 }
-
-//1.absolutes의 배열과 sings의 배열이 같은 부분에서 양,음수를 바꿔준다.
-//2.바뀐 값으로 누적값을 구한다.
