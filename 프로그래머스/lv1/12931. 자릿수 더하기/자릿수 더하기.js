2475d2836fdc279da7a29fc5aa88f66f209c6f15
lionloopy <121242536+lionloopy@users.noreply.github.com>
2023-02-01 23:01:28 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -1,11 +1,10 @@
 function solution(n)
 {
     var answer = 0;
-    let a = String(n)
-    //숫자가 아닌 문자로 돌려야 돌아가면서 한자리씩 더해줄 수 있다.
-    for(let i=0; i<a.length;i++){
-        answer += parseInt(a[i])
+    n = String(n)
+    for(let i=0; i<n.length; i++){
+        answer += parseInt(n[i])
     }
-    //다시 숫자로 바꿔주기
+
     return answer;
 }
