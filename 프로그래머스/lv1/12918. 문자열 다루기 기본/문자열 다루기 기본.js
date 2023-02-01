a4c209e1eacc3fc55be9c728a1b42ea79c9b10bb
lionloopy <121242536+lionloopy@users.noreply.github.com>
2023-02-01 22:44:50 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,8 +1,8 @@
 function solution(s) {
-    var answer
-    if((s.length == 4 || s.length ==6) && s == parseInt(s)){
+    var answer 
+    if((s.length ==4 || s.length == 6) && s == parseInt(s)){
         answer = true
-    }else {
+    }else{
         answer = false
     }
     return answer;
