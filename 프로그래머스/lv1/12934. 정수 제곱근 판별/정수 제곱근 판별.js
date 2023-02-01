38c73d32ff84ab9b27fdcdcd90abb500a32d70b5
lionloopy <121242536+lionloopy@users.noreply.github.com>
2023-02-01 23:14:47 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -1,6 +1,6 @@
 function solution(n) {
     var answer = 0;
     let a = Math.sqrt(n)
-    a %1 ==0? answer = Math.pow(a+1,2): answer = -1
+    a%1 == 0? answer = Math.pow(a+1,2): answer = -1
     return answer;
 }
